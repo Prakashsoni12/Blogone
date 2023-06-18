82f65736e74b020f62e840405f0d9270107c1564
Prakashsoni12 <85999331+Prakashsoni12@users.noreply.github.com>
2023-06-18 20:04:54 +0530

    Update settings.py
    sec_rem

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7rq71+688kyqf0x=2y3-xm$(fw2qb_aw(%z98&&%m+1r+%78!)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
